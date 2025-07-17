87891e9c1d8624b5a64d7f8805be9afc9ccb7af3
luhie <nerdrun.luhie@gmail.com>
2025-07-17 12:27:50 +0900

    Tutorials: Create module - #2 How to handle for error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,17 +1,23 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
 // 1.          2.      3.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	/*
 		var message string
 		message = fmt.Sprintf("Hi, %v. Welcom!", name)
+		nil =  No error
 	*/
-	return message
+	return message, nil
 }
 
 // 1.Function name, 2.Parameter type, 3.Retrun type
