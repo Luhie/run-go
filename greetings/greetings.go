5363b33ec41981382139395be8b8031087ed6174
luhie <nerdrun.luhie@gmail.com>
2025-07-22 16:40:53 +0900

    Tutorials: Create module - #3 Go map

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -23,6 +23,20 @@ func Hello(name string) (string, error) {
 
 // 1.Function name, 2.Parameter type, 3.Retrun type
 
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+
+}
+
 func randomFormat() string {
 	formats := []string{
 		"Hi, %v. Welcom!",
--- hi/hi.go
+++ hi/hi.go
@@ -11,7 +11,10 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Gladys")
+	names := []string{"Gladys", "Smantha", "Darrin"}
+
+	// message, err := greetings.Hello("Gladys")
+	message, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
